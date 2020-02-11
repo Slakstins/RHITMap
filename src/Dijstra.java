63c03c0b25f4c8983621f31f86d36fa9fa74aa89
kuznicmd <kuznicmd@rose-hulman.edu>
2020-02-11 15:18:26 -0500

    Can now draw nodes and edges

--- src/Dijstra.java
+++ src/Dijstra.java
@@ -1,9 +1,15 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
 import GUI.Edge;
 import GUI.Node;
+import javafx.scene.shape.Line;
 
 
 public class Dijstra 
@@ -28,13 +34,23 @@ public class Dijstra
 		private Node endNode;
 		private int cost;
 		private boolean outside;
+		private Line2D.Double edge;
 		
 		public Edge(Node startNode, Node endNode, int cost, boolean outside)
 		{
-			startNode = startNode;
-			endNode = endNode;
-			cost = cost;
-			outside = outside;
+			this.startNode = startNode;
+			this.endNode = endNode;
+			this.cost = cost;
+			this.outside = outside;
+			
+			this.edge = new Line2D.Double(new Point2D.Double(startNode.x+startNode.size/2, startNode.y+startNode.size/2), new Point2D.Double(endNode.x+endNode.size/2, endNode.y+endNode.size/2));
+			
+		}
+		
+		public void drawOn(Graphics2D g) {
+			g.setColor(Color.BLACK);
+			g.fill(this.edge);
+			g.draw(edge);
 		}
 		
 	}
@@ -42,15 +58,20 @@ public class Dijstra
 	{
 		private int x;
 		private int y;
+		private int size = 40;
 		private String name;
 		private ArrayList<Edge> edges;
 		
+		private Ellipse2D.Double node;
+		
 		public Node(int x, int y, String name, ArrayList<Edge> edges)
 		{
-			x = x;
-			y = y;
-			name = name;
-			edges = edges;
+			this.x = x;
+			this.y = y;
+			this.name = name;
+			this.edges = edges;
+			
+			this.node = new Ellipse2D.Double(x, y, size, size);
 		}
 		
 		public int getX()
@@ -64,6 +85,12 @@ public class Dijstra
 		public String getName()
 		{
 			return name;
+		}
+		
+		public void drawOn(Graphics2D g) {
+			g.setColor(Color.BLACK);
+			//g.fill(this.node);
+			g.draw(node);
 		}
 	}
 }
